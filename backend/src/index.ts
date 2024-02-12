30d500c818b3935f01458201700dded8ca7ecb0b
Meer Uzair <meer.uxair007@gmail.com>
2024-02-12 15:15:01 +0530

    added registration backend routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express, { Response, Request } from 'express'
 import cors from 'cors'
 import "dotenv/config"
 import mongoose from "mongoose"
-
+import userRoutes from './routes/users.route'
 const app = express();
 
 app.use(express.json())
@@ -11,10 +11,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(cors())
 
-app.get("/api/test", async (req: Request, res: Response) => {
-    res.json({ mes: "hello" })
-})
-
+app.use("/api/v1/users", userRoutes)
 
 
 const start = async () => {
