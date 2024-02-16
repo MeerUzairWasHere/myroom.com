ce38d048b8edc5f1252c7cad4d23405c13c48d1a
Meer Uzair <meer.uxair007@gmail.com>
2024-02-16 14:14:52 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,10 +20,10 @@ app.use(cors({
 
 app.use(express.static(path.join(__dirname, "../../frontend/dist")))
 
-app.use("/api/v1/auth", authRoutes)
-app.use("/api/v1/users", userRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
 
-app.get("*", (req, res) => {
+app.get("*", (req: Request, res: Response) => {
     res.sendFile(path.resolve(__dirname, "../../frontend/dist", "index.html"));
 });
 
