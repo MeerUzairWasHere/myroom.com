f633d9b7bac8d1a9769b24da3ed80de1a9f02f6a
Meer Uzair <meer.uxair007@gmail.com>
2025-02-03 02:25:23 +0530

    wip

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -69,7 +69,9 @@ const PORT = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI!);
+    await connectDB(
+      "mongodb+srv://meeruzairwashere:191123_Khair.@learningbackend.spoupnw.mongodb.net/MyRoom?retryWrites=true&w=majority"
+    );
     app.listen(PORT, () => {
       console.log("Server is running on port: " + PORT + "...");
     });
