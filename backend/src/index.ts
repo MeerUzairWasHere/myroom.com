0ca67be585e0937cfe02a0dadbd7fc917553a73a
Meer Uzair <meer.uxair007@gmail.com>
2025-02-03 02:32:17 +0530

    wip

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -72,7 +72,7 @@ const start = async () => {
     await connectDB(
       "mongodb+srv://meeruzairwashere:191123_Khair.@learningbackend.spoupnw.mongodb.net/MyRoom?retryWrites=true&w=majority"
     );
-    app.listen(PORT, () => {
+    app.listen(8000, () => {
       console.log("Server is running on port: " + PORT + "...");
     });
   } catch (error) {
